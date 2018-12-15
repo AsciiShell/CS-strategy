dcdd29920bc9547bf3a687a51919fe11795d19d0
Unknown <andrsolo21@gmail.com>
2018-12-15 23:50:28 +0300

    просо таблица

--- StrategyGUI/StrategyGUI/Form1.cs
+++ StrategyGUI/StrategyGUI/Form1.cs
@@ -17,6 +17,38 @@ namespace StrategyGUI
         public Form1()
         {
             InitializeComponent();
+            
+
+
+        }
+
+        public void DrawRectangleInt(PaintEventArgs e,int xn,int yn, int otx, int oty)
+        {
+
+            Pen blackPen = new Pen(Color.Black, 1);
+            int sx = (this.Size.Width -15- 2*otx) / xn;
+            int sy = (this.Size.Height - 39 - 2*oty) / yn;
+            for (int i = 0; i < xn; i++)
+            {
+                for (int j =0; j < yn; j++)
+                {
+                    e.Graphics.DrawRectangle(blackPen, otx + sx * i, oty + sy * j, sx, sy);
+                }
+            }          
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int xn = 10;
+            int yn = 10;
+            int otx = 15;
+            int oty = 30;
+            int h = this.Size.Height - 39;
+            int w = this.Size.Width;
+
+            PaintEventArgs args = new PaintEventArgs(this.CreateGraphics(), new Rectangle(0, 0, w, h));
+            DrawRectangleInt(args, xn, yn, otx,oty);
         }
     }
 }
