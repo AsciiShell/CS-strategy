77e3f9b318bfc351b478f2f5f4bc8b8ced2d27d8
Unknown <andrsolo21@gmail.com>
2018-12-16 00:43:32 +0300

    метод получения (i,j) кликнутой ячйеки

--- StrategyGUI/StrategyGUI/Form1.Designer.cs
+++ StrategyGUI/StrategyGUI/Form1.Designer.cs
@@ -30,31 +30,47 @@ namespace StrategyGUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // button3
             // 
-            this.button1.Location = new System.Drawing.Point(516, 89);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button3.Location = new System.Drawing.Point(0, 269);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(669, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(812, 306);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(716, 291);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
             this.Name = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- StrategyGUI/StrategyGUI/Form1.cs
+++ StrategyGUI/StrategyGUI/Form1.cs
@@ -15,10 +15,10 @@ namespace StrategyGUI
     public partial class Form1 : Form
     {
 
-        private int _xn = 10;
-        private int _yn = 5;
-        private int _otx = 15;
-        private int _oty = 30;
+        private int _xn;
+        private int _yn;
+        private int _otx;
+        private int _oty;
         private int _w;
         private int _h;
         private int _sx;
@@ -26,7 +26,11 @@ namespace StrategyGUI
 
         public Form1()
         {
-            InitializeComponent();            
+             _xn = 20;
+             _yn = 9;
+             _otx = 15;
+             _oty = 30;
+            InitializeComponent();
             _h = this.Size.Height - 39;
             _w = this.Size.Width;
             _sx = (this.Size.Width - 15 - 2 * _otx) / _xn;
@@ -48,18 +52,45 @@ namespace StrategyGUI
             
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            PaintEventArgs args = new PaintEventArgs(this.CreateGraphics(), new Rectangle(0, 0, _w, _h));
-            DrawRectangleInt(args);
-            drawInCell(args, 2, 2);
-        }
-
         public void drawInCell(PaintEventArgs e,int x,int y)
         {
             Pen blackPen = new Pen(Color.Blue, 5);
 
             e.Graphics.DrawRectangle(blackPen, _otx + _sx * x + _sx/3, _oty + _sy * y + _sy / 3, _sx/3, _sy/3);
         }
+
+       /* private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }*/
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PaintEventArgs args = new PaintEventArgs(this.CreateGraphics(), new Rectangle(0, 0, _w, _h));
+            DrawRectangleInt(args);
+            drawInCell(args, 2, 2);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x, y;
+            x = e.Location.X;
+            y = e.Location.Y;
+
+            if (x < _otx || y < _oty || x > _sx * (_xn + 1) || y > _sy * (_yn + 1))
+            {
+                label1.Text = "  ";
+            }
+            else
+            {
+                int iX = (x - _otx) / _sx;
+                int iY = (y - _oty) / _sy;
+                label1.Text = iX + "  " + iY;
+            }
+            
+                
+
+        }
     }
+    
 }
